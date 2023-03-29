afb44763b18551f3b954304a195161378c39c2eb
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-03-29 23:25:02 +0300

    update

--- src/scripts/refs.js
+++
@@ -1,7 +0,0 @@
-export const refs = {
-  userListEl: document.querySelector('.js-user-list'),
-  albumListEl: document.querySelector('.js-album-list'),
-  photoListEl: document.querySelector('.js-photo-list'),
-  modalEl: document.querySelector('.js-modal'),
-  backdropEl: document.querySelector('.backdrop'),
-};
